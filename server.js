ba9ff071e59d89f317f9d947500fa78de81ca81e
Camila Alm <camila.salmeidatech@hotmail.com>
2024-11-16 22:00:18 -0500

    last version

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');                                 // Add cors as a middleware.
 
-const petRouter = require('./controllers/pets.js')            // Import the controller file.
+const petRouter = require('./controllers/pets.js');            // Import the controller file.
 
 app.use(cors({
-    origin: 'http://localhost:5173'                           // White list.
-}))
+    origin: 'http://localhost:5173'                           // Whitelist.
+}));
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -19,7 +19,7 @@ mongoose.connection.on('connected', () => {
 
 app.use(express.json());
 
-app.use('/pets', petRouter)                               // Add the petRouter to the `/pets` route.
+app.use('/pets', petRouter);                               // Add the petRouter to the `/pets` route.
 
 app.listen(3000, () => {
   console.log('The express app is ready!🎧');
