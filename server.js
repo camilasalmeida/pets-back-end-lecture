11f06abb674e7148e529cd3667ce20505966ee75
Camila Alm <camila.salmeidatech@hotmail.com>
2024-11-16 19:00:12 -0500

    add cors, final version

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');                                 // Add cors as a middleware.
 
-const petRouter = require('./controllers/pets.js')            // import the controller file
+const petRouter = require('./controllers/pets.js')            // Import the controller file.
 
+app.use(cors({
+    origin: 'http://localhost:5173'                           // White list.
+}))
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -15,9 +19,7 @@ mongoose.connection.on('connected', () => {
 
 app.use(express.json());
 
-// Routes go here
-app.use('/pets', petRouter)                               // add the petRouter to the `/pets` route
-
+app.use('/pets', petRouter)                               // Add the petRouter to the `/pets` route.
 
 app.listen(3000, () => {
   console.log('The express app is ready!🎧');
